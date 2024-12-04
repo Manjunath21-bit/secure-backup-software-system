3b4c5197c104c1a2b5e3cfae0000fc82173c0e41
Manjunath G R <gtravindranath173@gmail.com>
2024-12-04 21:39:59 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     //this is the connection file
 $server= "localhost";
 $username="root";
-$password="";
+$password="1111";
 $con= mysqli_connect($server,$username,$password,'filemanager');
 if(!$con){
     die("Connection not made due to".mqsqli_connect_error());
